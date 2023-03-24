02ed2a46f28c6a99d9dd391a945f37d0d179fc02
Amir <amirosama21@gmail.com>
2023-03-24 05:29:10 -0700

    fixed 5-

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,10 @@ for (j = 0; j < 10; j++)
 int i;
 for (i = 0; i <= 14; i++)
 {
-    if (i > 9)
-    _putchar(i / 10 + '0');
+if (i > 9)
+{
+_putchar(i / 10 + '0');
+}
 _putchar(i % 10 + '0');
 }
 _putchar('\n');
