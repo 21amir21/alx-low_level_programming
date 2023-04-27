516402f95f6a43e5aa5e5f15a1d43dfb5dec6397
Amir <amirosama21@gmail.com>
2023-04-28 01:32:39 +0300

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,22 +1,6 @@
 #include "lists.h"
 
 /**
- * @brief another interesting way of doing it with recursion
- *
- * void free_list(list_t *head)
- * {
- *	if (head)
- *	{
- *		free_list(head->next);
- *		if (head->str)
- *			free(head->str);
- *		free(head);
- *	}
- * }
- *
- */
-
-/**
  * free_list - free a list
  * @head: the head of the list
  * Return: nothing
