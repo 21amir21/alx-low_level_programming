b7edb8f8e1f104eb5c66c8caebea734cc11a860d
Amir <amirosama21@gmail.com>
2023-05-02 02:45:32 +0300

    Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -19,8 +19,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	}
 
 	newNode->n = n;
-	newNode->next = head;
-	head = newNode;
+	newNode->next = *head;
+	*head = newNode;
 
 	return (newNode);
 }
