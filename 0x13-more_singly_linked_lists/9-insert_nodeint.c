c612f591a36ba15f495f88bc3737ac19252b8508
Amir <amirosama21@gmail.com>
2023-05-02 17:12:54 +0300

    fixing the mixed decleration

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,11 +13,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newNode = malloc(sizeof(listint_t));
 
-	newNode->n = n;
-
 	unsigned int i;
 
 	listint_t *temp = *head;
+
+	newNode->n = n;
 
 	if (*head == NULL || head == NULL)
 		return (NULL);
