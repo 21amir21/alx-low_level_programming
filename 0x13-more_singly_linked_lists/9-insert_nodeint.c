cff01ba17f4d6a69368bf88d1ffe5a75a4ae0d4b
Amir <amirosama21@gmail.com>
2023-05-03 22:25:22 +0300

    zx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -51,6 +51,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			newNode->next = *head;
 		}
+		head = *head;
 		*head = newNode;
 	}
 	else if (idx > listint_len(*head) || idx < i)
